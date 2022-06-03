bef8d820871321dbece4d338dfea247eb4ccd73b
Kacper Kałużny <kacper.kaluzny.2002@gmail.com>
2022-06-03 13:24:08 +0200

    first constat values

--- cBuilding.cs
+++ cBuilding.cs
@@ -8,14 +8,24 @@ namespace LiftSimulator
 {
     class cBuilding
     {
-
+        int iNumberOfLifts=1;
+        int iNumberOfFloors;
+        
     }
     class cFloor
     {
-
+        int iNumerOfFloor;
+        int[] iPassengers = new int[3];
     }
     class cLift
     {
+        int iPresentNumberOfPeopleInside=0;
+        int iMaxWeight;
+        int iMaxNumberOfPeopleInside;
 
+        static void vCalculatingTheNextFloor()
+        {
+
+        }
     }
 }
