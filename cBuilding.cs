71cfd7da6a283e2fa4e8bfd21ba9c8f2c9df26af
Kacper Kałużny <s188853@o365.student.pg.edu.pl>
2022-06-04 20:31:10 +0200

    new functions

--- cBuilding.cs
+++ cBuilding.cs
@@ -6,24 +6,60 @@ using System.Threading.Tasks;
 
 namespace LiftSimulator
 {
+    public struct sPassenger
+    {
+        public int iPresentFloor;
+        public int jTargetFloor;
+    }
+
     class cBuilding
     {
         int iNumberOfLifts=1;
         int iNumberOfFloors;
+         List<cFloor> lFloors = new List<cFloor>();
+        
+        static void vAddFloorsToBulding(int iNumberOfFloors)
+        {
+            
+            for(int i = 0; i < iNumberOfFloors; i++)
+            {
+               // lFloors.Add(new cFloor() { iNumerOfFloor=i});
+            }
+        }
         
     }
     class cFloor
     {
-        int iNumerOfFloor;
-        int[] iPassengers = new int[3];
+        public int iNumerOfFloor;
+        //int[] iPassengers = new int[3];
+
+        List<sPassenger> lPassengersOnTheFloor = new List<sPassenger>();
+
+         static void vAddPassengerToTheFloor(int iPresentFloor, int iTargettFloor)
+        {
+           
+         //   lPassengersOnTheFloor.Add(new sPassenger() { iPresentFloor=iPresentFloor, jTargetFloor=iTargettFloor });
+            
+        }
     }
     class cLift
     {
         int iPresentNumberOfPeopleInside=0;
         int iMaxWeight;
         int iMaxNumberOfPeopleInside;
+        bool bCurrentDirection=true; //0-down, 1-up
 
         static void vCalculatingTheNextFloor()
+        {
+
+        }
+
+        static void vAddPassengerToTheLift()
+        {
+
+        }
+
+        static void vAddPassengersToTheLift()
         {
 
         }
